575820850319d138cf4b19dc297cb04a1e3e2107
Lucas Paim <lucaspaim92@gmail.com>
2015-07-03 21:51:10 -0300

    atualizando projeto

--- Client/src/main/Main.java
+++ Client/src/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         Random r = new Random();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             new main.Escritor("arquivo" + r.nextInt(3) + ".txt");
             new main.Leitor("arquivo" + r.nextInt(3) + ".txt", 1, 100);
         }
